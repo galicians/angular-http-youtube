fae2f7f01c2f803a5950933715d0db5d67c0152e
galicians <galicians@gmail.com>
2020-05-13 15:27:23 +0100

    1 file modified

--- src/app/youtube-search.injectables.ts
+++ src/app/youtube-search.injectables.ts
@@ -5,8 +5,8 @@ import {
 } from './youtube-search.service';
 
 export const youTubeSearchInjectables: Array<any> = [
-  {provide: YouTubeSearchService, useClass: YouTubeSearchService},
-  {provide: YOUTUBE_API_KEY, useValue: YOUTUBE_API_KEY},
-  {provide: YOUTUBE_API_URL, useValue: YOUTUBE_API_URL},
+  // // {provide: YouTubeSearchService, useClass: YouTubeSearchService},
+  // {provide: YOUTUBE_API_KEY, useValue: YOUTUBE_API_KEY},
+  // {provide: YOUTUBE_API_URL, useValue: YOUTUBE_API_URL},
   {provide: YOUTUBE_API_URL, useValue: YOUTUBE_API_URL}
 ];
